46a9808466b197a09d94a24d1a563b33882e6dc8
hannalampinen <n0laha00@students.oamk.fi>
2021-09-30 12:16:11 +0300

    jod tehtävä

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+
+const URL = 'https://api.jokes.one/jod';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+  useEffect (() => {
+    async function getJoke() {
+      try {
+        const response = await fetch(URL);
+        if (response.ok) {
+          const json = await response.json();
+          const joke = json.contents.jokes[0].joke;
+          setTitle(joke.title);
+          setText(joke.text);
+        } else {
+          alert ('Error receiving the joke!');
+        }
+      } catch (err) {
+        alert(err);
+      }
+    }  
+    getJoke();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin:50}}>
+      <h1>Joke of the day</h1>
+      <h3>{title}</h3>
+      <p>{text}</p>
     </div>
   );
 }
